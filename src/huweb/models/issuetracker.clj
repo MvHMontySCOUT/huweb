95f972551f929bef6b1076bc63b0d916e07a5434
Martin van Haeften <mail@mvanhaeften.nl>
2012-06-11 20:56:17 +0200

    added writer for issuelog

--- src/huweb/models/issuetracker.clj
+++ src/huweb/models/issuetracker.clj
@@ -1,3 +1,6 @@
 (ns huweb.models.issuetracker)
 
-(defn writeissue! [issue])
+(defn writeissue! [issue]
+  (use 'clojure.java.io)
+ (with-open [wrtr (writer "test.txt" :append true)]
+  (.write wrtr "hoi")))
